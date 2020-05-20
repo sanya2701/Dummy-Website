9ae73d7980c8fca7294296085fc36c73d922b5fb
sanya <sanyagupta225@gmail.com>
2020-05-20 13:12:08 +0530

    initial

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const flash = require("connect-flash");
 const passport = require("passport");
+require('dotenv').config()
 
 const app = express();
 
